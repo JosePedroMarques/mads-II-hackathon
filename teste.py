3a9507798a451c9f1665c09496c917bf6a522c56
Jose Pedro Marques <ze.p.smarques@gmail.com>
2012-12-03 16:29:42 +0000

    unit test v7

--- teste.py
+++ teste.py
@@ -4,10 +4,11 @@ class MyTest(unittest.TestCase):
 
     def testMethod(self):
         self.assertEqual(1 + 2, 3, "1 + 2 not equal to 3")
+    def testMethod2(self):
+        self.assertEqual(1 + 2, 4, "1 + 2 not equal to 3")
 
 
-if __name__ == '__main__':
-        
+if __name__ == '__main__':      
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(MyTest))
     unittest.TextTestRunner(verbosity=2).run(suite)
